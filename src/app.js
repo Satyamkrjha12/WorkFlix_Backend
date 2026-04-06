1e43217cbbce5ec6c7245bc4a47b3fc4c0203a26
Satyamkrjha12 <154836014+Satyamkrjha12@users.noreply.github.com>
2026-04-06 14:00:37 +0530

    render FRONTEND Origin Updated

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "https://workflixwebapp.netlify.app",
-    "https://workflix-frontend.onrender.com/"
+    "https://workflix-frontend.onrender.com"
   ],
   credentials: true
 }));
