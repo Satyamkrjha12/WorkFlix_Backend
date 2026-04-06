1c78fcf840abe8bd1ec130412c181704445684ca
Satyamkrjha12 <154836014+Satyamkrjha12@users.noreply.github.com>
2026-04-06 14:39:34 +0530

    render FRONTEND Origin Updated

--- src/app.js
+++ src/app.js
@@ -2,30 +2,29 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
-
 const authRoutes = require("./routes/auth.routes");
 const gigRoute = require("./routes/gig.routes");
 const proposalRoute = require("./routes/proposal.routes");
 
 const app = express();
 
+// 🔥 MUST BE FIRST
+app.set("trust proxy", 1);
+
 app.use(cors({
   origin: "https://workflix-frontend.onrender.com",
   credentials: true
 }));
 
-app.options("*", cors());
+// OPTIONAL (remove if causing issue)
+// app.options("/*", cors());
+
 app.use(cookieParser());
 app.use(express.json());
-app.set("trust proxy", 1);
-
-
 
 app.use("/api/gig", gigRoute);
 app.use("/api/auth", authRoutes);
 app.use("/api/proposal", proposalRoute);
-
-
 
 app.get("/", (req, res) => {
   res.send("API running...");
